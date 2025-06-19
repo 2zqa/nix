1a63ac3f6c4a11eb76451dd677741513590f41df
Marijn <25235249+2zqa@users.noreply.github.com>
2025-06-19 14:11:13 +0200

    Clean up firefox.nix

--- firefox.nix
+++ firefox.nix
@@ -1,5 +1,3 @@
-{ config, pkgs, ... }:
-
 let
   lock-false = {
     Value = false;
@@ -25,7 +23,6 @@ in
         pref("mousewheel.default.delta_multiplier_y", 30);
       '';
 
-      # ---- POLICIES ----
       # Check about:policies#documentation for options.
       policies = {
         DisableTelemetry = true;
@@ -42,18 +39,14 @@ in
         DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
         DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
 
-        # ---- EXTENSIONS ----
-        # Check about:support for extension/add-on ID strings.
         # Valid strings for installation_mode are "allowed", "blocked",
         # "force_installed" and "normal_installed".
         ExtensionSettings = {
-          "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
           # Bitwarden
           "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
             installation_mode = "force_installed";
           };
-          # uBlock Origin
           "uBlock0@raymondhill.net" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
             installation_mode = "force_installed";
@@ -63,7 +56,6 @@ in
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/tab-shortcuts-2zqa/latest.xpi";
             installation_mode = "force_installed";
           };
-          # KeyHub
           "keyhub@topicus.nl" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/topicus-keyhub/latest.xpi";
             installation_mode = "force_installed";
