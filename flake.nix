518ca580e737c167a7cf8ff74a2731ecc249e4f6
Marijn <25235249+2zqa@users.noreply.github.com>
2025-07-04 21:25:08 +0200

    Fix packages not updating

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS System Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs =
