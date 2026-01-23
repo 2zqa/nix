a3f8e51b87149dff89ad2da5bc95e4cd5bf6cc72
Marijn <25235249+2zqa@users.noreply.github.com>
2026-01-23 15:30:39 +0100

    Add edex-ui

--- configuration.nix
+++ configuration.nix
@@ -202,6 +202,7 @@ in
     (callPackage ./derivations/avvie.nix { })
     (callPackage ./derivations/tarug.nix { })
     (callPackage ./derivations/dynamic-wallpaper.nix { })
+    (callPackage ./derivations/edex-ui.nix { })
 
     # development
     git
