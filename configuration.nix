2dc561e431fe399a927aea0fbc537933c4e172be
Marijn <25235249+2zqa@users.noreply.github.com>
2026-02-02 12:11:52 +0100

    Add bash aliases

--- configuration.nix
+++ configuration.nix
@@ -110,22 +110,34 @@ in
     ];
   };
 
-  programs.bash.promptInit = ''
-    # Provide a nice prompt if the terminal supports it.
-    if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
-        PROMPT_COLOR="1;31m"
-        ((UID)) && PROMPT_COLOR="1;32m"
-        if [ -n "$INSIDE_EMACS" ]; then
-        # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
-        PS1="\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
-        else
-        PS1="\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
-        fi
-        if test "$TERM" = "xterm"; then
-        PS1="\[\033]2;\h:\u:\w\007\]$PS1"
-        fi
-    fi
-  '';
+  programs.bash = {
+    shellAliases = {
+      ls = "ls --color --group-directories-first";
+      wl-copy = "wl-copy --trim-newline";
+      o = "xdg-open .";
+      enix = "vim $HOME/nix/configuration.nix";
+      emake = "(cd $HOME/nix; make)";
+      eupdate = "(cd $HOME/nix; make update)";
+    };
+
+    promptInit = ''
+      # Provide a nice prompt if the terminal supports it.
+      if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
+          PROMPT_COLOR="1;31m"
+          ((UID)) && PROMPT_COLOR="1;32m"
+          if [ -n "$INSIDE_EMACS" ]; then
+          # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+          PS1="\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+          else
+          PS1="\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+          fi
+          if test "$TERM" = "xterm"; then
+          PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+          fi
+      fi
+    '';
+  };
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
