9f928b3e62170c5555406ae6ecfe572d543b7a6f
Marijn <25235249+2zqa@users.noreply.github.com>
2026-09-17 23:40:49 +0200

    Use latest Linux kernel

--- configuration.nix
+++ configuration.nix
@@ -57,8 +57,12 @@
   };
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
 
   networking.hostName = "lonepine";
 
